ebe94a3e9710c9ccc8f5cf1f3e94c8129573619e
lyubovbrnsk <brnsssskl@gmail.com>
2023-02-15 23:33:40 +0500

    add min and average methods

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,17 +1,35 @@
 public class SalesManager {
-    protected long[] sales;
+    protected int[] sales;
 
-    public SalesManager(long[] sales) {
+    public SalesManager(int[] sales) {
         this.sales = sales;
     }
 
-    public long max() {
-        long max = -1;
-        for (long sale : sales) {
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
+    }
+
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (min > sale) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int average() {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return (sum - (min() + max())) / (sales.length - 2);
     }
 }
